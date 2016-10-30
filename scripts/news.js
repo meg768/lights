dcd96acd606bbf6fc81080cf5fa25848da4f9aeb
meg768 <magnus@egelberg.se>
2016-10-30 23:25:46 +0100

    -

--- scripts/news.js
+++ scripts/news.js
@@ -97,8 +97,60 @@ var Module = module.exports = function() {
 	};
 
 
-	function listen() {
+	function displayClock(priority) {
 
+
+		function hslToRgb(h, s, l) {
+
+		    var r, g, b;
+
+		    if (s == 0){
+		        r = g = b = l; // achromatic
+		    } else{
+		        var hue2rgb = function hue2rgb(p, q, t){
+		            if(t < 0) t += 1;
+		            if(t > 1) t -= 1;
+		            if(t < 1/6) return p + (q - p) * 6 * t;
+		            if(t < 1/2) return q;
+		            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+		            return p;
+		        }
+
+		        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		        var p = 2 * l - q;
+		        r = hue2rgb(p, q, h + 1/3);
+		        g = hue2rgb(p, q, h);
+		        b = hue2rgb(p, q, h - 1/3);
+		    }
+
+		    return {red:Math.round(r * 255), green:Math.round(g * 255), blue:Math.round(b * 255)};
+
+		}
+
+		var now   = new Date();
+		var hue   = ((now.getHours() % 12) * 60 + now.getMinutes()) / 2;
+		var color = hslToRgb(hue / 360.0, 1, 0.5);
+
+		var options = {};
+		options.textColor = sprintf('rgb(%d, %d, %d)', color.red, color.green, color.blue);
+		options.text      = sprintf('%02d:%02d', now.getHours(), now.getMinutes());
+		options.priority  = priority;
+
+		matrix.emit('text', options);
+
+	}
+
+	function scheduleClock(callback) {
+		var rule = new Schedule.RecurrenceRule();
+		rule.minute = new Schedule.Range(0, 59, 1);
+
+		Schedule.scheduleJob(rule, function() {
+			displayClock('low');
+		});
+	}
+
+	function listen() {
+		scheduleClock();
 
 		_newsSwitch.on('ON', function() {
 			_newsSwitch.pauseEvents(2000);
@@ -117,6 +169,7 @@ var Module = module.exports = function() {
 
 		_newsSwitch.on('OFF', function() {
 			matrix.emit('stop');
+			displayClock();
 		});
 
 
