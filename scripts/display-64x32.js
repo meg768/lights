fb1e1a71c93d0a2f5d504c744d3285ceabce606c
meg768 <magnus@egelberg.se>
2016-11-12 10:42:24 +0100

    -

--- scripts/display-64x32.js
+++ scripts/display-64x32.js
@@ -5,8 +5,6 @@ var random     = require('yow').random;
 var isArray    = require('yow').isArray;
 var isString   = require('yow').isString;
 
-var tellstick  = require('./tellstick.js');
-
 var ClockAnimation  = require('./clock-animation.js');
 var QuotesAnimation = require('./quotes-animation.js');
 var NewsAnimation   = require('./news-animation.js');
@@ -15,30 +13,37 @@ var Animator        = require('./animator.js');
 
 var Display = function(url) {
 
-	var _matrix          = require('socket.io-client')(url);
-	var _animations      = [];
+	var _matrix = require('socket.io-client')(url);
 
 	function run() {
 		console.log('Display active:', url);
 
+		var connected = false;
+		var animations = [];
+
+		animations.push(new ClockAnimation(_matrix));
+		animations.push(new NewsAnimation(_matrix));
+		animations.push(new ClockAnimation(_matrix));
+		animations.push(new QuotesAnimation(_matrix));
+
+		var animator = new Animator(animations);
+
 		_matrix.on('connect', function() {
-
 			console.log('Connected to display', url);
-
-			var animations = [];
-
-			animations.push(new ClockAnimation(_matrix));
-			animations.push(new NewsAnimation(_matrix));
-			animations.push(new ClockAnimation(_matrix));
-			animations.push(new QuotesAnimation(_matrix));
-
-			var animator = new Animator(animations);
-
-			_matrix.on('idle', function() {
-				animator.runNextAnimation();
-			});
-
+			connected = true;
 		});
+
+		_matrix.on('disconnect', function() {
+			console.log('Disconnected from display', url);
+			connected = false;
+		});
+
+		_matrix.on('idle', function() {
+			if (connected)
+				animator.runNextAnimation();
+		});
+
+
 	};
 
 	run();
