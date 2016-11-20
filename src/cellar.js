b0b465dfdcf1c8fe26d36fee27f4acc35cc1e06c
meg768 <magnus@egelberg.se>
2016-11-20 17:37:55 +0100

    -

--- src/cellar.js
+++ src/cellar.js
@@ -39,7 +39,7 @@ var Module = function() {
 			_matrix.stopAnimations();
 
 			// Activate automatically after a time
-			_autoActivationTimer.setTimer(1000 * 60 * 60 * 0.05, function() {
+			_autoActivationTimer.setTimer(1000 * 60 * 60 * 8, function() {
 				setLightsActive(true);
 			});
 
