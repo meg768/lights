ebd7d506c3976daa32d3138b576c586b2dc2db1d
meg768 <magnus@egelberg.se>
2016-11-20 17:31:47 +0100

    -

--- src/matrix.js
+++ src/matrix.js
@@ -5,9 +5,10 @@ var isString   = require('yow/is').isString;
 
 var Matrix = module.exports = function(url, animators) {
 
+	var _this = this;
+
 	var _animations = [];
 	var _index = -1;
-	var _this = this;
 	var _running = false;
 
 	_this.socket = require('socket.io-client')(url);
@@ -27,39 +28,27 @@ var Matrix = module.exports = function(url, animators) {
 		_this.socket.emit('emoji', {id:435, pause:1, priority:'!'});
 	};
 
+	function runAnimation(animation, priority) {
+		animation.run(priority).then(function() {
+
+		})
+		.catch(function(error) {
+			console.log('Animation failed.', error);
+		});
+	};
+
 	function runNextAnimation(priority) {
 
-		function runLater() {
-			setTimeout(function() {
-				_this.runNextAnimation(priority);
-			}, 60000);
-		}
-
-		function runAnimation(animation, priority) {
-			animation.run(priority).then(function() {
-			})
-			.catch(function(error) {
-				console.log('Animation failed.', error);
-
-				runLater();
-			});
-		};
-
-		var now  = new Date();
-		var time = sprintf('%02d:%02d', now.getHours(), now.getMinutes());
-
-		if (_animations.length > 0 && time >= '08:00' && time <= '23:59') {
+		if (_running && _animations.length > 0) {
 			_index = (_index + 1) % _animations.length;
 			runAnimation(_animations[_index], priority);
-		}
-		else {
-			runLater();
 		}
 	}
 
 	function run() {
 		console.log(sprintf('Matrix display %s active.', url));
 
+		// Create animators
 		animators.forEach(function(animator) {
 			_animations.push(new animator(_this));
 		});
@@ -81,9 +70,7 @@ var Matrix = module.exports = function(url, animators) {
 		});
 
 		_this.socket.on('idle', function() {
-			if (_running) {
-				runNextAnimation('low');
-			};
+			runNextAnimation('low');
 		});
 
 	};
