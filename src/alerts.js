73e39d499c6c1c743992103b8ebf023c8cc934cd
meg768 <magnus@egelberg.se>
2017-01-08 21:50:57 +0100

    -

--- src/alerts.js
+++ src/alerts.js
@@ -102,7 +102,7 @@ var Module = function() {
 		_switch.on('ON', function() {
 			if (!_active) {
 				_active = true;
-				_Xawake  = true;
+				_awake  = true;
 
 				console.log('Alerts activated.');
 				alert('Larm aktiverat.')
